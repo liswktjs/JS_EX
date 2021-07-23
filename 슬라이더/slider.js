b329957752a713f4d53246914182205f956fbc04
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-07-23 23:12:07 +0900

    fin

--- 슬라이더/slider.js
+++ 슬라이더/slider.js
@@ -1,3 +1,6 @@
+//영화 정보를 담아 놓은 리스트 
+//제목과 포스터 이미지 그리고 순위를 표기한다 
+//순위를 알아야 순위대로 정렬할 수 있으므로
 const movie_list = [
     {
         title: '블랙위도우',
@@ -53,57 +56,93 @@ const movie_list = [
 
 const boxoffice_list = document.querySelector('.boxoffice_list');
 
-const printPoster = (boxoffice_list,movie_list) => {
+//화면상에 포스터들을 보여지게 한다 
+const printPoster = (boxoffice_list, movie_list) => {
     const left_btn = document.querySelector('.left_btn');
     const right_btn = document.querySelector('.right_btn');
+    
+    //각각의 p0,p1...이 현재 표기하고 있는 포스터의 랭킹을 표기하는 리스트
+    let p_index = [
+        1,2,3,4,5
+    ];
 
-    let currentIndex = 0; 
+    const p0 = document.querySelector('.p0');
+    const p1 = document.querySelector('.p1');
+    const p2 = document.querySelector('.p2');
+    const p3 = document.querySelector('.p3');
+    const p4 = document.querySelector('.p4');
 
-    let p0 = document.querySelector('.p0');
-    let p1 = document.querySelector('.p1');
-    let p2 = document.querySelector('.p2');
-    let p3 = document.querySelector('.p3');
-    let p4 = document.querySelector('.p4');
+    const ps_index = [
+        p0,p1,p2,p3,p4
+    ];
 
+    //DOM이 로드되면 p0,p1..을 차례대로 방문하여 
+    //각각의 p0,p1..에 movie_list 정보에 따라 img 10개를 삽입한다 
     document.addEventListener('DOMContentLoaded', () => {
         for(let i =0; i<5; i++){
-            let p = '.p' + String(i);
-            document.querySelector(p).innerHTML = `
+            ps_index[i].innerHTML = `
             ${movie_list.map( e => 
                 `<img src="${e.poster}"
                 alt = "${e.title}"
                 data-id = "${e.ranking}"
                 class = "poster${e.ranking}" />`
             ).join('')}`;
-            
-            /*movie_list.map(function(e, index) {
-                let img =document.createElement('img');
-                let c = 'poster' + String(index);
-                img.setAttribute('src',e.poster);
-                img.setAttribute('alt',e.title);
-                img.classList.add(c);
-                img.setAttribute('data-id',e.ranking);
-                
-                //img.setAttribute('z-index', -1);
-                document.querySelector(p).appendChild(img);
-            });*/
-            
         }
-        p0.querySelector('.poster1').style.zIndex = '999';
-        p1.querySelector('.poster2').style.zIndex = '999';
-        p2.querySelector('.poster3').style.zIndex = '999';
-        p3.querySelector('.poster4').style.zIndex = '999';
-        p4.querySelector('.poster5').style.zIndex = '999'; 
+        
+        //처음 로드되었을 때 1등 2등 3등 4등 5등 순서대로 표기하기 위해서 zIndex를 활용해서 처리해준다
+        for (let i =0; i < 5; i++) {
+            ps_index[i].querySelector('.poster' + String(p_index[i])).style.zIndex = '999';
+        } 
+        
+        
     });
 
+    //왼쪽 화살표를 클릭할때에 첫번째 1등 영화가 있던 것이 10등 영화가 보이게 처리하는 방식
     left_btn.onclick = () => {
-        
-    };
-
+        for (let i =0; i < 5; i++){
+            //원래 표기 하던 것을 보여지지 않게 zIndex를 수정해준다
+            ps_index[i].querySelector('.poster' + String(p_index[i])).style.zIndex = '-1';
+            p_index[i] = plus(p_index[i]);
+            ps_index[i].querySelector('.poster'+ String(p_index[i])).style.zIndex = '999';
+        }  
+    };                                                                                 
+    //오른쪽 화살표를 클릭할 때에 첫번째 1등 영화가 있던 것이 2등 영화가 보이게 처리하는 방식
     right_btn.onclick = () => {
+        for (let i =0; i < 5; i++) {
+            ps_index[i].querySelector('.poster' + String(p_index[i])).style.zIndex = '-1';
+            p_index[i] = minus(p_index[i]);
+            ps_index[i].querySelector('.poster' + String(p_index[i])).style.zIndex = '999';
+        }
 
     };
 
+    //1등부터 10등까지의 영화만을 보여줄 것이므로 만약 이들을 가리키는 index가 
+    //10이라면 1로 바꾸어주고 아니라면 +1를 해준다 
+    function plus(e){
+        if (e == 10) {
+            e = 1;
+        }
+        else{
+            e += 1;
+        }
+        return e;
+    }
+    //위와 마찬가지의 이유로 만약 index가 1이라면 10으로 바꾸어주고 
+    //아니라면 -1를 해준다 
+    function minus(e){
+        if (e == 1){
+            e = 10;
+        }else{
+            e -= 1;
+        }
+        return e;
+    }
+    //만일 유저가 블랙위도우 포스터를 클릭했을 경우 새로운 링크로 이동하도록 처리 한다  
+    boxoffice_list.onclick = ({target}) => {
+        if('블랙위도우' == target.alt){
+            location.href = './link.html';
+        }
+    }
 }
 
 printPoster(boxoffice_list,movie_list);
