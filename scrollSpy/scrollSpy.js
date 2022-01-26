c1568b95e9527e579fa99e72c8487a0decac8a1e
JASUN LEE <liswktjs@gamil.com>
2022-01-26 22:21:06 +0900

    reafactor: js 함수를 모듈화함

--- scrollSpy/scrollSpy.js
+++ scrollSpy/scrollSpy.js
@@ -1,23 +1,37 @@
-const navElem = document.querySelector('.nav');
-const navItems = Array.from(navElem.children);
-const contentElem = document.querySelector('.container');
-const contentItems = Array.from(contentElem.children);
-const offsetTops = contentItems.map((elem) => {
+export default function scrollSpy(){
+    
+    const navElem = document.querySelector('.nav');
+    const navItems = Array.from(navElem.children);
+    const contentElem = document.querySelector('.container');
+    const contentItems = Array.from(contentElem.children);
+    const offsetTops = contentItems.map((elem) => {
     const [ofs, clh] = [elem.offsetTops , elem.clientHeight];
     return [ofs- clh / 2, ofs + clh / 2]; 
-})
-
-window.addEventListener('scroll', (e) =>{
-    const {scrollTop} = e.target.scrollingElement;
-})
-
-navElem.addEventListener('click', (e) => {
-    const targetElem = e.target;
-    if(targetElem.tagName === 'BUTTON'){
-        const targetIndex = navItems.indexOf(targetElem.parentElement);
-        contentItems[targetIndex].scrollIntoView({
-            block: 'start',
-            behavior: 'smooth',
-        });
+    })
+    
+    this.init = () => {
+        scrollEvent();
+        navEvent();
+        
     }
-})
+    const scrollEvent = () => {
+        window.addEventListener('scroll', (e) => {
+            const {scrollTop} = e.target.scrollingElement;
+        })
+    }
+    const navEvent = () => {
+        navElem.addEventListener('click', (e) => {
+            const targetElem = e.target;
+            if(targetElem.tagName === 'BUTTON'){
+                const targetIndex = navItems.indexOf(targetElem.parentElement);
+                contentItems[targetIndex].scrollIntoView({
+                    block: 'start',
+                    behavior: 'smooth',
+                })
+            }
+        })
+    }
+}
+
+const s = new scrollSpy();
+s.init();
