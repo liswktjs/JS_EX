41a081a27c99b6b417ef2eb60ccd32a773c501c4
JASUN LEE <liswktjs@gamil.com>
2022-01-26 20:42:16 +0900

    style: 💄 스타일링작업

--- scrollSpy/scrollSpy.js
+++ scrollSpy/scrollSpy.js
@@ -1,8 +1,23 @@
-export default function scrollSpy(){
-    this.init = () => {
-        
-    }
-}
+const navElem = document.querySelector('.nav');
+const navItems = Array.from(navElem.children);
+const contentElem = document.querySelector('.container');
+const contentItems = Array.from(contentElem.children);
+const offsetTops = contentItems.map((elem) => {
+    const [ofs, clh] = [elem.offsetTops , elem.clientHeight];
+    return [ofs- clh / 2, ofs + clh / 2]; 
+})
 
-const s = new scrollSpy();
-s.init();
+window.addEventListener('scroll', (e) =>{
+    const {scrollTop} = e.target.scrollingElement;
+})
+
+navElem.addEventListener('click', (e) => {
+    const targetElem = e.target;
+    if(targetElem.tagName === 'BUTTON'){
+        const targetIndex = navItems.indexOf(targetElem.parentElement);
+        contentItems[targetIndex].scrollIntoView({
+            block: 'start',
+            behavior: 'smooth',
+        });
+    }
+})
